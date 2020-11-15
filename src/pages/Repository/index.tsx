752d484c4e0139a40442b399df382b1d2af6bf5f
Joao Rangel <joao.vran@gmail.com>
2020-11-15 00:03:03 -0300

    repository routes creation

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import { useRouteMatch } from 'react-router-dom';
+
+interface RepositoryParams {
+  repository: string;
+}
 
 const Repository: React.FC = () => {
-  return <h1>Repository</h1>;
+  const { params } = useRouteMatch<RepositoryParams>();
+
+  // eslint-disable-next-line react/jsx-one-expression-per-line
+  return <h1>Repository - {params.repository}</h1>;
 };
 
 export default Repository;
